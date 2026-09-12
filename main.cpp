6f58d73c276bbf569031a8d4cd6c4a7c5db4e5d6
Aryan gupta <aryangupta93860@gmail.com>
2026-09-12 09:46:43 -0500

    feat: add formatted file information table
    Added a formatted CLI table for displaying scanned file information.
    Used iomanip manipulators such as setw, left, right, and setfill
    to align filename, extension, and file size columns.

--- main.cpp
+++ main.cpp
@@ -1,33 +1,80 @@
 #include <iostream>
 #include <filesystem>
+#include <vector>
+#include <iomanip>
 using namespace std;
 namespace fs = std::filesystem;
 
+class FileInfo
+{
+    string filename;
+    string extension;
+    long long size;
+
+public:
+    FileInfo(string f, string e, long long s) : filename(f), extension(e), size(s)
+    {
+    }
+    string getfilename() const
+    {
+        return filename;
+    }
+    string getextension() const
+    {
+        return extension;
+    }
+    long long getsize() const
+    {
+        return size;
+    }
+    void display()
+    {
+        cout << setw(25) << left << filename;
+        cout << setw(15) << extension;
+        cout << setw(15) <<right<< size;
+        cout << endl;
+    }
+};
+
 int main()
 {
+    // startup
     cout << "=============================" << endl;
     cout << "      |PC GUARDIAN v1.0|     " << endl;
     cout << "=============================" << endl;
     cout << "ENTER DIRECTORY PATH : ";
     string path;
     getline(cin, path);
+
+    // intitialization
     fs::directory_entry Directory{path};
+    vector<FileInfo> files;
 
     if (Directory.exists())
     {
         if (Directory.is_directory())
         {
+            cout<<"\033[33m";
             cout << "ITS A FOLDER DIRECTORY" << endl;
+            cout<<"\033[0m";
             for (const auto &entry : fs::directory_iterator(path))
             {
-                if(entry.is_regular_file()){
-                    cout << "THE FILE NAME IS : " <<entry.path().filename() << endl;
-                    cout <<"EXTENSION : "<< entry.path().extension() << endl;
-                    cout << "FILE SIZE : " << entry.file_size() << " BYTES" << endl;
+                if (entry.is_regular_file())
+                {
+                    auto filename = entry.path().filename().string();
+                    auto extension = entry.path().extension().string();
+                    auto size = entry.file_size();
+
+                    FileInfo file(filename, extension, size);
+
+                    // obj data storing
+                    files.push_back(file);
                 }
-                else{
-                cout << "THE FOLDER NAME IS : " <<entry.path().filename() << endl;
-            }}
+                else
+                {
+                    cout << "THE FOLDER AVALIBLE IN PATH: " << entry.path().filename() << endl;
+                }
+            }
         }
         else if (Directory.is_regular_file())
         {
@@ -43,6 +90,21 @@ int main()
     {
         cout << "DIRECTORY DOES NOT EXIST!!" << endl;
     }
-    cout << "YOUR PATH :  " << path << endl;
+    // displaying file info
+    cout<<"\033[32m";
+    cout<<"\nTHE FILES AVALIBE IN THE PATH :\n\n";
+    cout<<"\033[0m";
+    // heading code
+    cout << setw(25) << left << "Name";
+        cout << setw(15) << "Extension";
+        cout << setw(15) <<right<< " Size";
+        cout << endl;
+        cout << setfill('-') << setw(55) << "-" << endl;
+        cout << setfill(' ');
+    for (auto &Storedfile : files)
+    {
+        Storedfile.display();
+    }
+    cout << "\nYOUR PATH :  " << path << endl;
     return 0;
 }
